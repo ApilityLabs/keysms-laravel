a69f09954d56c77ff02d8413a4501c9da12d11f1
Thomas Alrek <thomas@alrek.no>
2024-09-12 15:22:56 +0200

    Add UrlRoutable interface

--- src/Contact.php
+++ src/Contact.php
@@ -6,12 +6,13 @@ use Stringable;
 use KeySMS\Traits\Rest;
 use KeySMS\Contracts\PhoneNumber as PhoneNumberContract;
 use Brick\PhoneNumber\PhoneNumber;
+use Illuminate\Contracts\Routing\UrlRoutable;
 
 /**
  * @property ?string $name
  * @property-read ?string $phone
  */
-class Contact implements Stringable, PhoneNumberContract
+class Contact implements Stringable, PhoneNumberContract, UrlRoutable
 {
     use Rest;
 
@@ -69,6 +70,55 @@ class Contact implements Stringable, PhoneNumberContract
     public function getInternationalPhoneNumber(): string
     {
         return $this->phone;
+    }
+
+    /**
+     * Get the value of the model's route key.
+     *
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param string  $value
+     * @param string|null  $field
+     * @return static|null
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        if ($field !== 'id') {
+            return null;
+        }
+
+        return static::find(static::find($value));
+    }
+
+    /**
+     * Retrieve the child model for a bound value.
+     *
+     * @param string $childType
+     * @param mixed $value
+     * @param string|null $field
+     * @return null
+     */
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        return null;
     }
 
     public function __toString(): string
